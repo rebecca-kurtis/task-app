27dd9fc91935ff4a1b406f26c0d90ab859c43253
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-08-24 23:09:26 -0700

    Add user seed data

---
+++ db/seeds/01_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (first_name, last_name, email, password)
+VALUES ('John', 'Doe', 'johndoe@example.com', 'password123');
+
+INSERT INTO users (first_name, last_name, email, password)
+VALUES ('Anne', 'Smith', 'asmith@example.com', 'password123');
