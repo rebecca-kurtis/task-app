748b1481db164467f314a7188eb212e8c4d22a4b
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-09-28 22:38:18 -0700

    Add folder for NavBar components

---
+++ components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useState } from "react";
+
+import { StyleSheet } from 'react-native';
+import { IndexPath, Layout, Select, SelectItem, Text } from '@ui-kitten/components';
+
+export default function NavBar() {
+
+  return (
+    
+    <View>
+      <Text>Temporary Nav Bar Content</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  selectContainer: {
+    minHeight: 128,
+  },
+});
